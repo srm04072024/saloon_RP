574da9d9f5cb559f481469ac38178da4d309e335
srm04072024 <s.r.m.20.3.2018@gmail.com>
2025-03-25 03:52:56 +0530

    fix:add spacing to left part of appointment section

--- src/app/sections/Appointment.tsx
+++ src/app/sections/Appointment.tsx
@@ -7,19 +7,21 @@ export default function Appointment() {
       <div className="absolute inset-0 bg-gradient-to-b from-black/90 via-black/70 to-black/60"></div>
       <div className="absolute inset-0 grid grid-cols-6 px-5 md:px-15 py-25">
         <div className="col-span-6 md:col-span-3">
-          <div>
-            <span>&#x2B50;</span>
-            <span>&#x2B50;</span>
-            <span>&#x2B50;</span>
-            <span>&#x2B50;</span>
-            <span>&#x2B50;</span>
-          </div>
-          <h2 className="text-3xl font-bold">
-            We Are Best Barbers & Hair Cutting Salon at Jajpur.
-          </h2>
-          <div>
-            <h4>Appointment</h4>
-            <h2 className=" text-2xl font-bold">9999999999</h2>
+          <div className="h-full w-full flex flex-col gap-8">
+            <div>
+              <span>&#x2B50;</span>
+              <span>&#x2B50;</span>
+              <span>&#x2B50;</span>
+              <span>&#x2B50;</span>
+              <span>&#x2B50;</span>
+            </div>
+            <h2 className="text-3xl font-bold">
+              We Are Best Barbers & Hair Cutting Salon at Jajpur.
+            </h2>
+            <div className="flex flex-col gap-4">
+              <h4>Appointment</h4>
+              <h2 className=" text-2xl font-bold">9999999999</h2>
+            </div>
           </div>
         </div>
         <div className="col-span-6 md:col-span-3 ">
