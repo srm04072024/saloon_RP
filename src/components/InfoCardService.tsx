8f055923f3302322eea98e4ee271ded9b7df89b6
srm04072024 <s.r.m.20.3.2018@gmail.com>
2025-03-26 00:26:22 +0530

    fix:bugs on service styling effect

--- src/components/InfoCardService.tsx
+++ src/components/InfoCardService.tsx
@@ -22,8 +22,8 @@ export default function InfoCardService({ service }: propsType) {
         />
       </div>
       <div className="absolute inset-0 bg-gradient-to-b from-transparent via-transparent to-black group-hover:from-black/60 group-hover:via-black/50 group-hover:to-black/60"></div>
-      <div className="absolute inset-0 translate-y-[85%] flex flex-col items-center gap-6 transition-all duration-500 group-hover:translate-y-[45%] px-6">
-        <div className="pb-6 text-center border-b-[1.5px] border-brandcolor w-fit group-hover:w-[90%] transition-all duration-700 ease-in-out">
+      <div className="absolute inset-0 translate-y-[92%] sm:translate-y-[85%] md:translate-y-[80%] flex flex-col items-center gap-6 transition-all duration-500 group-hover:translate-y-[45%] px-6">
+        <div className="pb-6 text-center border-b-[1.5px] border-brandcolor w-fit group-hover:w-[90%] transition-all duration-700 ease-in-out mb-4">
           <h1 className=" text-3xl font-bold text-white">
             {service.serviceName}
           </h1>
