e1ab01c7992a3c495ed3983643c22be11baec161
Soumya Ranjan Maharana <s.r.m.20.3.2018@gmail.com>
2025-07-05 00:01:53 +0530

    feat: add all sections

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,13 @@
 import Appointment from "@/sections/Appointment";
 import BarberPricing from "@/sections/BarberPricing";
-// import BarbersSection from "@/sections/BarbersSection";
-// import CustomerReview from "@/sections/CustomerReview";
+import BarbersSection from "@/sections/BarbersSection";
+import CustomerReview from "@/sections/CustomerReview";
 import Experience from "@/sections/Experience";
 import ExtraServices from "@/sections/ExtraService";
-// import Features from "@/sections/Features";
+import Features from "@/sections/Features";
 import HeroSection from "@/sections/HeroSection";
 import PortFolio from "@/sections/PortFolio";
-// import PromoVideo from "@/sections/PromoVideo";
+import PromoVideo from "@/sections/PromoVideo";
 import Services from "@/sections/Services";
 
 export default function Home() {
@@ -17,11 +17,11 @@ export default function Home() {
       <PortFolio />
       <Experience />
       <Services />
-      {/* <CustomerReview /> */}
-      {/* <Features /> */}
-      {/* <PromoVideo /> */}
+      <CustomerReview />
+      <Features />
+      <PromoVideo />
       <BarberPricing />
-      {/* <BarbersSection /> */}
+      <BarbersSection />
       <ExtraServices />
       <Appointment />
     </main>
